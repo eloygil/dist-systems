8d61386ecd79cbce6738f1b236f1fa72653f7e72
Luis Delicado <l.delicado33@gmail.com>
2016-12-27 17:23:26 +0100

    Random subset for client

--- opty-source-files/client.erl
+++ opty-source-files/client.erl
@@ -1,8 +1,11 @@
 -module(client).
 -export([start/5]).
 
+-define(size, 3).
+
 start(ClientID, Entries, Reads, Writes, Server) ->
     spawn(fun() -> open(ClientID, Entries, Reads, Writes, Server, 0, 0) end).
+    %spawn(fun() -> open(ClientID, rand:uniform(Entries-size), Reads, Writes, Server, 0, 0) end).
 
 open(ClientID, Entries, Reads, Writes, Server, Total, Ok) ->
     Server ! {open, self()},
@@ -43,14 +46,18 @@ do_transaction(ClientID, Entries, Reads, Writes, Handler) ->
 do_read(Entries, Handler) ->
     Ref = make_ref(),
     Num = rand:uniform(Entries),
+    %Num = rand:uniform(size),
     Handler ! {read, Ref, Num},
+    %Handler ! {read, Ref, Entries+Num},
     receive
         {value, Ref, Value} -> Value
     end.
 
 do_write(Entries, Handler, Value) ->
     Num = rand:uniform(Entries),
+    %Num = rand:uniform(size),
     Handler ! {write, Num, Value}.
+    %Handler ! {write, Entries+Num, Value}.
 
 do_commit(Handler) ->
     Ref = make_ref(),
